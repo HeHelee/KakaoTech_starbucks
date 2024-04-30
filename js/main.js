502bf9b64867ddb44f286972ea94516ead365dc9
Kuheewon <selly3109@naver.com>
2024-04-30 10:14:34 +0900

    feat Visual 기능 완성

--- js/main.js
+++ js/main.js
@@ -18,6 +18,30 @@ searchInputEl.addEventListener('blur', function () {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function() {
-    console.log('scroll!');
+window.addEventListener('scroll', _.throttle(function() {
+  console.log('scroll!');
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    //배지 숨기기, css가지고만 충분히 알 수 있음.
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  }else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display : 'block'
+    })
+  }
+},300));
+//_.throttle(함수,시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl,1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
 });
